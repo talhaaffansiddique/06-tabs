87cc44a3998d549a4dc7c32edbf4c6dfa63c7ce0
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-05-15 09:57:08 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import BtnContainer from "./BtnContainer";
+import JobInfo from "./JobInfo";
 
 const url = "https://www.course-api.com/react-tabs-project";
 
-const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
+function App() {
+  const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
-
-  // current items
+  const [currentItem, setCurrentItem] = useState(0);
 
   const fetchJobs = async () => {
     const response = await fetch(url);
     const newJobs = await response.json();
     setJobs(newJobs);
-    setIsLoading(false);
+    setLoading(false);
   };
-
   useEffect(() => {
     fetchJobs();
   }, []);
+  if (loading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+  return (
+    <section className="jobs-center">
+      {/* btn container */}
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      {/* job info */}
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
+}
 
-  console.log(jobs);
-
-  return <h2>Tabs Starter</h2>;
-};
 export default App;
---
+++ src/Duties.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
+
+const Duties = ({ duties }) => {
+  const id = uuidv4();
+  console.log(id);
+  return (
+    <div>
+      {duties.map((duty, index) => {
+        return (
+          <div key={id} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Duties;
